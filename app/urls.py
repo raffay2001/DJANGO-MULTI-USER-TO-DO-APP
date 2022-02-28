faf0a3e4d8864ada83f8822e6b1cbafae7b9723f
raffay2001 <raffay.ahmed009@gmail.com>
2022-02-28 15:31:36 +0500

    second commit

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 
-
 app_name = 'app'
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('login/', views.login, name = 'login'),
     path('signup/', views.signup, name = 'signup'),
+    path('logout/', views.logout, name = 'logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login as loginUser, logout as logoutUser
 
 # Create your views here.
 
@@ -9,11 +11,56 @@ def home(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        form = AuthenticationForm()
+        context = {
+            "form" : form,
+        }
+        return render(request, 'login.html', context=context)
+    else:
+        form = AuthenticationForm(data=request.POST)
+        context = {
+            "form" : form,
+        }
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            print(user)
+            if user is not None:
+                loginUser(request, user)
+                return HttpResponseRedirect(reverse('app:home'))
+        else:
+            return render(request, 'login.html', context=context)
+
+
+
 
 def signup(request):
-    form = UserCreationForm()
-    context = {
-        "form": form,
-    }
-    return render(request, 'signup.html', context=context)
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context = {
+            "form" : form,
+        }
+        return render(request, 'signup.html', context=context)
+    else:
+        print(f'{request.POST}')
+        form = UserCreationForm(request.POST)
+        context = {
+            "form": form,
+        }
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            if user is not None:
+                redirect_url = reverse('app:login')
+                print(f'The Reversed URL is: "{redirect_url}"')
+                return HttpResponseRedirect(redirect_url)
+
+        else:
+            return render(request, 'signup.html', context=context)
+
+
+def logout(request):
+    logoutUser(request)
+    
